fd44e05989c49f8e21858ca132945e64561dc668
MM1152 <albert6306@naver.com>
2020-07-02 19:55:12 +0900

    Project Upload

---
+++ Practice/Assets/Script/RoomMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMove : MonoBehaviour
+{
+    public Vector2 cameraChange;
+    public Vector3 playerChange;
+    private CameraMovement cam;
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            cam.minPosition += cameraChange;
+            cam.maxPosition += cameraChange;
+            collision.transform.position += playerChange;
+        }
+    }
+}
